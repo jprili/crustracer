4614ba5558fb46f271d8cb0679ea4f85ba1a5aed
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-05-25 11:45:42 -0700

    add ppm generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+const WIDTH:  i32 = 256;
+const HEIGHT: i32 = 256;
+const NORM:   f32 = 255.999;  
+
 fn main() {
-    println!("Hello, world!");
+    println!("P3\n{WIDTH} {HEIGHT}\n255");
+
+    for j in 0..WIDTH {
+        for i in 0..HEIGHT {
+            let r = (i as f32) / ((WIDTH - 1) as f32);
+            let g = (j as f32) / ((WIDTH - 1) as f32);
+            let b = 0.0; 
+
+            let ir: i32 = (NORM * r) as i32;
+            let ig: i32 = (NORM * g) as i32;
+            let ib: i32 = (NORM * b) as i32;
+
+            println!("{ir} {ig} {ib}")
+        }
+    }
 }
