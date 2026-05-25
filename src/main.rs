536285bae22b23aac7f9dbe64d6538d84bc56e64
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-05-25 12:08:28 -0700

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use log::{ info };
+use std::io;
+use std::io::Write;
+
 const WIDTH:  i32 = 256;
 const HEIGHT: i32 = 256;
 const NORM:   f32 = 255.999;  
@@ -6,6 +10,8 @@ fn main() {
     println!("P3\n{WIDTH} {HEIGHT}\n255");
 
     for j in 0..WIDTH {
+        info!("\rScanlines remaining: {}", HEIGHT - j);
+        match io::stderr().flush() { _ => () };
         for i in 0..HEIGHT {
             let r = (i as f32) / ((WIDTH - 1) as f32);
             let g = (j as f32) / ((WIDTH - 1) as f32);
@@ -18,4 +24,6 @@ fn main() {
             println!("{ir} {ig} {ib}")
         }
     }
+
+    info!("\rDone. \n")
 }
